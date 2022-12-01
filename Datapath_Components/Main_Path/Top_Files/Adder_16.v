89c21b9c016f7d63403afb13e7250f3b295a02a8
carpenterjaeden <112436035+carpenterjaeden@users.noreply.github.com>
2022-12-01 15:07:16 -0700

    idk

--- Datapath_Components/Main_Path/Top_Files/Adder_16.v
+++ Datapath_Components/Main_Path/Top_Files/Adder_16.v
@@ -1,1 +1,44 @@
-//top file for 16 parallel adders
+//8th operation used for calculating generate_window
+`timescale 1ns / 1ps
+
+module Adder_16(target_window,
+				In1,In2,In3,In4,In5,In6,In7,In8,In9,In10,In11,In12,In13,In14,In15,In16,
+                Out1,Out2,Out3,Out4,Out5,Out6,Out7,Out8,Out9,Out10,Out11,Out12,Out13,Out14,Out15,Out16);
+
+	input [1:0] check_wcol_out; // control bits for ALU operation
+                      
+	input signed [31:0] A, B;	    // inputs
+
+	output reg [31:0] Out;	// answer
+	
+
+        
+
+		Adder32Bit Adder32Bit_1(target_window, 4, Out1);
+		Adder32Bit Adder32Bit_2(target_window, 8, Out2);
+		Adder32Bit Adder32Bit_3(target_window, 12, Out3);
+		Adder32Bit Adder32Bit_4(target_window, 16, Out4);
+		//First four
+		Adder32Bit Adder32Bit_5(target_window, 20, Out5);
+		Adder32Bit Adder32Bit_6(target_window, 24, Out6);
+		Adder32Bit Adder32Bit_7(target_window, 28, Out7);
+		Adder32Bit Adder32Bit_8(target_window, 32, Out8);
+		//second four
+		Adder32Bit Adder32Bit_9(target_window, 36, Out9);
+		Adder32Bit Adder32Bit_10(target_window, 40, Out10);
+		Adder32Bit Adder32Bit_11(target_window, 44, Out11);
+		Adder32Bit Adder32Bit_12(target_window, 48, Out12);
+		//third four
+		Adder32Bit Adder32Bit_13(target_window, 52, Out13);
+		Adder32Bit Adder32Bit_14(target_window, 56, Out14);
+		Adder32Bit Adder32Bit_15(target_window, 60, Out15);
+		Adder32Bit Adder32Bit_16(target_window, 64, Out16);
+		//fourth four
+
+        //possible take from a different register than a1, that would require resetting the register after each 
+        //completed loop, and Out16 can be stored into it intermediately.
+
+
+
+	
+endmodule
--- Datapath_Components/Main_Path/Top_Files/Memmory_16_Generate.v
+++ Datapath_Components/Main_Path/Top_Files/Memmory_16_Generate.v
@@ -1,7 +1,7 @@
 //8th operation used for calculating generate_window
 `timescale 1ns / 1ps
 
-module Memmory_16_Generate(Out1,Out2,Out3,Out4,Out5,Out6,Out7,Out8,Out9,Out10,Out11,Out12,Out13,Out14,Out15,Out16);
+module Memmory_16_Generate(Clk, Out1,Out2,Out3,Out4,Out5,Out6,Out7,Out8,Out9,Out10,Out11,Out12,Out13,Out14,Out15,Out16);
 
 	input [1:0] check_wcol_out; // control bits for ALU operation
                       
@@ -10,25 +10,25 @@ module Memmory_16_Generate(Out1,Out2,Out3,Out4,Out5,Out6,Out7,Out8,Out9,Out10,Ou
 	output reg [31:0] Out;	// answer
 	
 
-        DataMemory DataMemory_1(Out, 0, Clk, 0, 1, ReadData1);
-        DataMemory DataMemory_2(Out, 0, Clk, 0, 1, ReadData2);
-        DataMemory DataMemory_3(Out, 0, Clk, 0, 1, ReadData3);
-        DataMemory DataMemory_4(Out, 0, Clk, 0, 1, ReadData4);
+        DataMemory DataMemory_1(Out1, 0, Clk, 0, 1, ReadData1);
+        DataMemory DataMemory_2(Out2, 0, Clk, 0, 1, ReadData2);
+        DataMemory DataMemory_3(Out3, 0, Clk, 0, 1, ReadData3);
+        DataMemory DataMemory_4(Out4, 0, Clk, 0, 1, ReadData4);
 
-        DataMemory DataMemory_5(Out, 0, Clk, 0, 1, ReadData5);
-        DataMemory DataMemory_6(Out, 0, Clk, 0, 1, ReadData6);
-        DataMemory DataMemory_7(Out, 0, Clk, 0, 1, ReadData7);
-        DataMemory DataMemory_8(Out, 0, Clk, 0, 1, ReadData8);
+        DataMemory DataMemory_5(Out5, 0, Clk, 0, 1, ReadData5);
+        DataMemory DataMemory_6(Out6, 0, Clk, 0, 1, ReadData6);
+        DataMemory DataMemory_7(Out7, 0, Clk, 0, 1, ReadData7);
+        DataMemory DataMemory_8(Out8, 0, Clk, 0, 1, ReadData8);
 
-        DataMemory DataMemory_9(Out, 0, Clk, 0, 1, ReadData9);
-        DataMemory DataMemory_10(Out, 0, Clk, 0, 1, ReadData10);
-        DataMemory DataMemory_11(Out, 0, Clk, 0, 1, ReadData11);
-        DataMemory DataMemory_12(Out, 0, Clk, 0, 1, ReadData12);
+        DataMemory DataMemory_9(Out9, 0, Clk, 0, 1, ReadData9);
+        DataMemory DataMemory_10(Out10, 0, Clk, 0, 1, ReadData10);
+        DataMemory DataMemory_11(Out11, 0, Clk, 0, 1, ReadData11);
+        DataMemory DataMemory_12(Out12, 0, Clk, 0, 1, ReadData12);
 
-        DataMemory DataMemory_13(Out, 0, Clk, 0, 1, ReadData13);
-        DataMemory DataMemory_14(Out, 0, Clk, 0, 1, ReadData14);
-        DataMemory DataMemory_15(Out, 0, Clk, 0, 1, ReadData15);
-        DataMemory DataMemory_16(Out, 0, Clk, 0, 1, ReadData16);
+        DataMemory DataMemory_13(Out13, 0, Clk, 0, 1, ReadData13);
+        DataMemory DataMemory_14(Out14, 0, Clk, 0, 1, ReadData14);
+        DataMemory DataMemory_15(Out15, 0, Clk, 0, 1, ReadData15);
+        DataMemory DataMemory_16(Out16, 0, Clk, 0, 1, ReadData16);
         
 
 
